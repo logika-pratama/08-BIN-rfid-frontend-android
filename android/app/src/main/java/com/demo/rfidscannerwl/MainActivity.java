bb3726900e3ae93fa8ea0eb70b313b45184d8b35
luky <lukyahmad81@gmail.com>
2022-10-04 18:00:56 +0700

    change package name and bundle id

--- android/app/src/main/java/com/demo/rfidscannerwl/MainActivity.java
+++ android/app/src/main/java/com/demo/rfidscannerwl/MainActivity.java
@@ -1,4 +1,4 @@
-package com.polrirfidfrontendandroid;
+package com.demo.rfidscannerwl;
 
 import com.facebook.react.ReactActivity;
 import android.os.Bundle;
--- android/app/src/main/java/com/demo/rfidscannerwl/MainApplication.java
+++ android/app/src/main/java/com/demo/rfidscannerwl/MainApplication.java
@@ -1,4 +1,4 @@
-package com.polrirfidfrontendandroid;
+package com.demo.rfidscannerwl;
 
 import android.app.Application;
 import android.content.Context;
@@ -65,7 +65,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.polrirfidfrontendandroid.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.demo.rfidscannerwl.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
